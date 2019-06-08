7f4db3df8d37ac1be0c1410869204bd5abbafccf
joabeleonard <joabe.leonard.feitosa@gmail.com>
2019-06-08 18:27:00 -0300

    Graficos

--- src/fornecedores/Relatorios.js
+++ src/fornecedores/Relatorios.js
@@ -9,41 +9,11 @@ import { getDataReportAvaliacoes, getDataReportVendas } from '../util/APIUtils';
   
   const data = [
     {
-      name: 'Jan', Quant: 29, Valor: 433, amt: 2400,
+      name: 'Jan', quant: 29, valor: 433
     },
     {
-        name: 'Fev', Quant: 35, Valor: 544, amt: 2400,
-      },
-    {
-      name: 'Mar', Quant: 45, Valor: 734, amt: 2210,
-    },
-    {
-      name: 'Abr', Quant: 52, Valor: 655, amt: 2290,
-    },
-    {
-      name: 'Mai', Quant: 77, Valor: 788, amt: 2000,
-    },
-    {
-      name: 'Jun', Quant: 93, Valor: 833, amt: 2181,
-    },
-    {
-      name: 'Jul', Quant: 45, Valor: 874, amt: 2500,
-    },
-    {
-      name: 'Ago', Quant: 59, Valor: 766, amt: 2100,
-    },
-    {
-      name: 'Set', Quant: 99, Valor: 923, amt: 2100,
-    },
-    {
-        name: 'Out', Quant: 222, Valor: 732, amt: 2100,
-      },
-      {
-        name: 'Nov', Quant: 322, Valor: 2222, amt: 2100,
-      },
-      {
-        name: 'Dez', Quant: 290, Valor: 2343, amt: 2100,
-      },
+        name: 'Fev', quant: 35, valor: 544
+      }
   ];  
 
   const data01 = [
@@ -67,7 +37,6 @@ class Relatorios extends Component{
     }
     
     loadDataReportVendas(){
-      console.log("dss");
       let promise;
          promise = getDataReportVendas();
        
@@ -80,10 +49,13 @@ class Relatorios extends Component{
          });
          promise            
          .then(response => {
-             const categorias = this.state.categorias.slice();
+             console.log(response);
              this.setState({
                  reportVendas: response
-             })
+             });
+             this.setState({
+              isLoading: false
+          })
  
          }).catch(error => {
              console.log(error);
@@ -135,7 +107,7 @@ class Relatorios extends Component{
                 <BarChart
                     width={500}
                     height={300}
-                    data={data}
+                    data={this.state.reportVendas}
                     margin={{
                       top: 5, right: 30, left: 20, bottom: 5,
                     }}
@@ -145,15 +117,16 @@ class Relatorios extends Component{
                   <YAxis />
                   <Tooltip />
                   <Legend />
-                  <Bar dataKey="Quant" fill="#8884d8" />
-                  <Bar dataKey="Valor" fill="#82ca9d" />
+                  <Bar dataKey="quant" fill="#8884d8" />
+                  <Bar dataKey="valor" fill="#82ca9d" />
                 </BarChart>
                 </div> 
 
                  <h1 className="page-title">Avaliação das Empresas</h1>               
                 <div className="new-poll-content">
                   <PieChart width={400} height={400}>
-                    <Pie dataKey="value" isAnimationActive={false} data={this.state.reportAvaliacoes} cx={200} cy={200} outerRadius={80} fill="#8884d8" label />
+                    <Pie dataKey="value" isAnimationActive={false} 
+                    data={this.state.reportAvaliacoes} cx={200} cy={200} outerRadius={80} fill="#8884d8" label />
                     <Tooltip />
                   </PieChart>
                 </div>   
