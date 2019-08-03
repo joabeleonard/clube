7c492377430e45d42e2b799c5236d195d1496d24
joabeleonard <joabe.leonard.feitosa@gmail.com>
2019-08-03 18:53:49 -0300

    Game

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,6 @@
-export const API_BASE_URL = 'http://localhost:5000/api';
+//export const API_BASE_URL = 'http://localhost:5000/api';
+
+export const API_BASE_URL = 'https://boon-back.herokuapp.com/api';
 //export const API_BASE_URL = '/api';
 export const ACCESS_TOKEN = 'accessToken';
 
